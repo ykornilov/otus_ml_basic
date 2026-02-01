27919e38a235da892ef7c5e40b5721bf1d5ceb5e
Yury Kornilov <kornilovyv@yandex-team.ru>
2026-02-02 00:36:58 +0300

    homework_05: OOP Advanced

---
+++ hw_05/__init__.py
@@ -0,0 +1,13 @@
+"""
+Домашнее задание: Пишем классы и плодим наследников
+"""
+
+from . import base, car, engine, exceptions, plane
+
+__all__ = [
+    "base",
+    "car",
+    "engine",
+    "exceptions",
+    "plane",
+]
